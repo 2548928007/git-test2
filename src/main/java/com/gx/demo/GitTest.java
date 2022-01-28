330122cbe130b664c531842b076847329f863486
Eric <2548928007@qq.com>
2022-01-28 16:55:49 +0800

    push到GitHub上

--- src/main/java/com/gx/demo/GitTest.java
+++ src/main/java/com/gx/demo/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello Git2");
         System.out.println("master冲突提交");
         System.out.println("dev冲突提交");
+        System.out.println("即将推送的新代码");
     }
 }
