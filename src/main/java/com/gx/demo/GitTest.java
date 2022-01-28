666e472f9fc3024975516ec46a66e925f10896d8
Eric <2548928007@qq.com>
2022-01-28 16:06:36 +0800

    IDEA第二次提交

---
+++ src/main/java/com/gx/demo/GitTest.java
@@ -0,0 +1,11 @@
+package com.gx.demo;
+
+/**
+ * @author Eric
+ * @create 2022-01-28 16:04
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello Git");
+    }
+}
