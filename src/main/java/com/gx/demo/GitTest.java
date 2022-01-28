af7104ee3336ef88ae44912aad514ce8e340f1f2
Eric <2548928007@qq.com>
2022-01-28 16:22:44 +0800

    冲突合并提交3

--- src/main/java/com/gx/demo/GitTest.java
+++ src/main/java/com/gx/demo/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello Git");
         System.out.println("hello Git2");
-        System.out.println("hello Git3");
+        System.out.println("dev冲突提交");
     }
 }
